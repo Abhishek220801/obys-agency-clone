3eb27331794a1d4c43282a80340f4ec3fa469226
Abhishek <abhi.sankhwar22@gmail.com>
2024-12-01 17:06:56 +0530

    page1 complete

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-var tl = gsap.timeline();
+function locoScroll(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+function loadingAnimation(){
+    var tl = gsap.timeline();
 
 tl.from(".line h1",{
     y: 150,
@@ -11,7 +43,7 @@ tl.from("#line1-p1",{
     onStart:function(){
         var h5timer = document.querySelector('#line1-p1 h5')
         var grow = 0
-        var timerId = setInterval(function(){
+        setInterval(function(){
             if(grow<100){
                 h5timer.innerHTML = grow++;
             }  
@@ -26,12 +58,6 @@ tl.to(".line h2",{
     opacity:1
 })
 
-tl.to("#loader",{
-    opacity:0,
-    duration:0.2,
-    delay:4,
-    display:"none"
-})
 tl.from("#page1",{
     y:1600,
     duration:0.5,
@@ -39,3 +65,30 @@ tl.from("#page1",{
     opacity:0,
     ease:Power4
 })
+tl.to("#loader",{
+    opacity:0,
+    duration:0.2,
+    delay:0,
+    display:"none"
+})
+
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+}
+
+loadingAnimation();
+locoScroll()
+
+document.addEventListener('mousemove',(dets)=>{
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+
+function magneticWords(){
+    Shery.makeMagnet("#nav-right h4");
+}
+magneticWords()
